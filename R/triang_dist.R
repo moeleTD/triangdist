85b8ca1faa221eeee47788015d6c0b9b29de5924
moeleTD <moele@alumni.uv.es>
2026-04-17 19:23:30 +0200

    Hemos preparado la conexión con Git para poder empezar a trabajar.

---
+++ R/triang_dist.R
@@ -0,0 +1,9 @@
+#Derivar la altura $h$: Recuerda que el área bajo la curva debe ser 1.Programar las funciones: dtriang, ptriang, qtriang y rtriang.Documentar: Usa #' @param, #' @return y #' @export encima de cada función para que roxygen2 genere el manual automáticamente.
+
+
+
+
+
+
+
+
